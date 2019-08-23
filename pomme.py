0ce1c142552a3106ad549e9660d2f628968ccb96
riptoc <riptoc@protonmail.com>
2019-08-23 12:12:43 +0100

    Added timer info output on start

--- pomme.py
+++ pomme.py
@@ -9,15 +9,26 @@ import argparse
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(
-    description="Pomme, a super simple pomodoro CLI.")
-parser.add_argument('timer',
+    description="Pomme: A super simple pomodoro CLI.")
+parser.add_argument('interval',
                     type=int,
                     nargs='?',
                     default=25,
-                    help='How long the timers last before a pause')
+                    help='How long the intervals last before a pause')
 parser.add_argument('pause',
                     type=int,
                     nargs='?',
                     default=3,
-                    help='How long the pauses last between timers')
+                    help='How long the pauses last between intervals')
 args = parser.parse_args()
+
+
+# Main program loop
+def main():
+    print("Timer starting. Each interval will last "
+          + str(args.interval) + " minutes, with a "
+          + str(args.pause) + " minute pause between them.\n")
+
+
+# Run the program
+main()
