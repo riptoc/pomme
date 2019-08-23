23e9df5590518c3856a542d1d61a28f53e265478
riptoc <riptoc@protonmail.com>
2019-08-23 14:42:22 +0100

    Extracted arg parsing and info text to functions

--- pomme.py
+++ pomme.py
@@ -7,27 +7,43 @@
 # Imports
 import argparse
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(
-    description="Pomme: A super simple pomodoro CLI.")
-parser.add_argument('interval',
-                    type=int,
-                    nargs='?',
-                    default=25,
-                    help='How long the intervals last before a pause')
-parser.add_argument('pause',
-                    type=int,
-                    nargs='?',
-                    default=3,
-                    help='How long the pauses last between intervals')
-args = parser.parse_args()
 
-
-# Main program loop
+# Main program init function
 def main():
+    # Parse command line args
+    args = getArgs()
+
+    interval = args.interval
+    pause = args.pause
+
+    # Print info about current timer
+    printInfo(interval, pause)
+
+
+def getArgs():
+    # Parse command line arguments and set up
+    # options/help etc.
+    parser = argparse.ArgumentParser(
+        description="Pomme: A super simple pomodoro CLI.")
+    parser.add_argument('interval',
+                        type=int,
+                        nargs='?',
+                        default=25,
+                        help='How long the intervals last before a pause')
+    parser.add_argument('pause',
+                        type=int,
+                        nargs='?',
+                        default=3,
+                        help='How long the pauses last between intervals')
+    args = parser.parse_args()
+    return args
+
+
+def printInfo(interval, pause):
+    # Print out timer information
     print("Timer starting. Each interval will last "
-          + str(args.interval) + " minutes, with a "
-          + str(args.pause) + " minute pause between them.\n")
+          + str(interval) + " minutes, with a "
+          + str(pause) + " minute pause between them.\n")
 
 
 # Run the program
